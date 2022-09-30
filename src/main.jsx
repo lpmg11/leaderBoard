305799d832d84d0af9cef7c97a4bd7494ddd7168
Luis Manrique <lpmg11manrique@gmail.com>
2022-09-30 13:20:26 -0600

    fixing document

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
-
-ReactDOM.createRoot(document.getElementById('main')).render(
-  <React.StrictMode>
-    <div style={{marginTop: '60px',
-                 display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-  }}>hola</div>
-  </React.StrictMode>
-)
